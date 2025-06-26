6a31b9745f47f4d390fd32e2817bcf2c36f3ab85
Marcilio  Jr <marcilio3@gmail.com>
2025-06-26 10:32:49 -0300

    gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f1e82ed9'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f7a8l499hj8"
+    "revision": "0.uvr7gq9agjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
